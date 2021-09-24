83945c5b6e17e6c211a57440107d31ad79b4379d
Ryan Miville <rmiville@securityscorecard.io>
2021-09-24 15:59:54 -0400

    rename command and add quickOpen code (not hooked up yet)

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	context.subscriptions.push(
-		vscode.commands.registerCommand('helloworld.helloWorld', () => {
+		vscode.commands.registerCommand('scoverage.toggleCoverage', () => {
 			// The code you place here will be executed every time your command is executed
 			// Display a message box to the user
 			// vscode.window.showInformationMessage('Hello World from HelloWorld!');
